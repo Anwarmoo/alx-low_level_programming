d3c3dc6d52b25ef332e887f6e51e037737e8d554
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-14 14:02:01 -0800

    1. I sometimes suffer from insomnia.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,16 @@
 
 #include<stdio.h>
 
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
 int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
