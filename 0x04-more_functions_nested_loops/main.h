c094b16975008124c6b97bc20e3b8d2a8522c516
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-15 16:08:16 -0800

    1. isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,12 @@
  * Return: true or false
  */
 int _isupper(int c);
+/**
+ * _isdigit - checks if it is digit
+ * @c: character to check
+ *
+ * Return: returns boolean
+ */
+int _isdigit(int c);
 
 #endif
