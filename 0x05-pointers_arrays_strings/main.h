7c148e112bc48c42b5a8da6203c0d909a00c6359
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-19 15:05:23 -0800

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,5 +19,12 @@ void reset_to_98(int *n);
  * @b: second integer
  */
 void swap_int(int *a, int *b);
+/**
+ * _strlen - length of string
+ * @s: string
+ *
+ * Return: returns length
+ */
+int _strlen(char *s);
 
 #endif
