1a46b8ecd7a184e8aaab8d360b20b54e7bf16345
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-19 15:12:52 -0800

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -26,5 +26,11 @@ void swap_int(int *a, int *b);
  * Return: returns length
  */
 int _strlen(char *s);
+/**
+ * _puts - prints something with new line
+ *
+ * @str: string to put
+ */
+void _puts(char *str);
 
 #endif
