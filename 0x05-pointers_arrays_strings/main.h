a9a45890c7c96db5644f520d4f2b0c419dae8faa
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-19 14:52:57 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * reset_to_98 - resets variable to 98
+ *
+ * @n: variable with pointer
+ */
+void reset_to_98(int *n);
+
+#endif
