02a9fe98a0b32c0ce03c63246c1683f3531270e6
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-22 12:01:38 -0800

    1. strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,13 @@ int _putchar(char c);
  * Return: returns char value
  */
 char *_strcat(char *dest, char *src);
+/**
+ * _strncat - concat string at n times
+ * @dest: destination source
+ * @src: source of array
+ * @n: n amount of times
+ * Return: return char value
+ */
+char *_strncat(char *dest, char *src, int n);
 
 #endif
