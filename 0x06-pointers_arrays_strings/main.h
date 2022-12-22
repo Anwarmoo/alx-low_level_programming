072480fe6232c68eeabeb7388470b2a486a92092
MOHAMED ADAN <anwarmoh389@gmail.com>
2022-12-22 11:56:18 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * _strcat - concats strings
+ * @dest: destination of string
+ * @src: source of string to concat
+ *
+ * Return: returns char value
+ */
+char *_strcat(char *dest, char *src);
+
+#endif
