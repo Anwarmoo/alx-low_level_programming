072b5b074f4620ccbfca7c58270f815bf229865f
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-01-30 14:21:30 -0800

    Task 00

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list of type list_t
+ * @h: Pointer to a singly linked list of type list_t.
+ * Return: numbers of nodes
+ *
+ * Description: print the elements of one list
+ **/
+
+size_t print_list(const list_t *h)
+{
+
+	unsigned int cont_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		}
+		cont_nodes++;
+	}
+	return (cont_nodes);
+}
