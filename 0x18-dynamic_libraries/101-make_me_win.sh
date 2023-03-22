8b894d36d2a0e24cb4bb64f1a2885e5a1d4a4d3f
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-03-22 15:55:56 -0700

    Task 03

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/101-make_me_win.so https://github.com/Anwarmoo/alx-low_level_programming/raw/0bc2962df4635675421e229c5bbf67429d54e33d/0x18-dynamic_libraries/101-make_me_win.so
-export LD_PRELOAD=$HOME/101-make_me_win.so
+wget -P /tmp https://github.com/Anwarmoo/alx-low_level_programming/raw/0bc2962df4635675421e229c5bbf67429d54e33d/0x18-dynamic_libraries/101-make_me_win.so
+export LD_PRELOAD=/tmp/101-make_me_win.so
